bccd77f33e602076f6b6056a7e5e7b6874917b35
emirardagn <arda.gun.24752@ozu.edu.tr>
2023-12-27 23:08:34 +0300

    now we can see our paintings on sell

--- btstore_frontend/src/artist/productPage.js
+++ btstore_frontend/src/artist/productPage.js
@@ -3,20 +3,33 @@ import { Link } from 'react-router-dom';
 import './ProductPage.css';
 
 function ProductPage() {
-    var symbol = /"([^"]+)"/;
-    var catching = document.cookie.match(symbol);
-    
-    //check artist is logged in
-    if (catching && catching.length > 1) {
-        var artistID = catching[1];
-    } else {
-        window.location.href = '/';
-    }
     const [products, setProducts] = useState([]);
+    const [artistID, setArtistID] = useState(null);
 
     useEffect(() => {
-        // Burada API'dan ürünleri çekiyoruz
-        
+        const fetchData = async () => {
+            try {
+                const symbol = /"([^"]+)"/;
+                const catching = document.cookie.match(symbol);
+                if (catching && catching.length > 1) {
+                    const artistID = catching[1];
+                    setArtistID(artistID);
+                    const response = await fetch(`http://localhost:3000/artist/id/${artistID}`);
+                    if (!response.ok) {
+                        throw new Error('Error');
+                    }
+                    const data = await response.json();
+                    setProducts(data.paintings);
+                } else {
+                    document.cookie = null
+                    window.location.href = '/';
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchData();
     }, []);
 
     const editProduct = (productId) => {
@@ -27,21 +40,25 @@ function ProductPage() {
         // Ürün silme mantığı burada
     };
 
+    if (artistID === null) {
+        // artistID mevcut değilse, burada bir şeyler yapabilirsiniz (örneğin, başka bir sayfaya yönlendirme)
+        return null;
+    }
+
     return (
         <div className="ProductPage-artist">
-            <h1>Products</h1>
+            <h1>My Paintings</h1>
+            <Link to="/createNewProduct">Sell New Painting</Link>
             <div className="ProductList-artist">
                 {products.map(product => (
                     <div key={product.id} className="ProductItem-artist">
                         <p>{product.name}</p>
-                        <p>Stok: {product.stock}</p>
                         <p>Fiyat: {product.price} TL</p>
                         <button onClick={() => editProduct(product.id)}>Edit</button>
                         <button onClick={() => removeProduct(product.id)}>Remove</button>
                     </div>
                 ))}
             </div>
-            <Link to="/createNewProduct">Add New Product</Link>
         </div>
     );
 }

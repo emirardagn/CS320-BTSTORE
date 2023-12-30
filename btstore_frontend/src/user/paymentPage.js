8dc1701d141aedaec2ae9ebe6ff4bef5e30cf96b
berketuncerozu <berke.tuncer@ozu.edu.tr>
2023-12-31 02:02:50 +0300

    added total price text on payment page

--- btstore_frontend/src/user/paymentPage.js
+++ btstore_frontend/src/user/paymentPage.js
@@ -1,7 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './user.css';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function PaymentPage() {
+
+
+  var symbol = /"([^"]+)"/;
+  var catching = document.cookie.match(symbol);
+  if (catching && catching.length > 1) {
+    var userID = catching[1];
+    
+  } else {
+    window.location.href = '/';
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3000/users/id/" + userID)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        document.getElementById("username").innerText = "Total Price: "+data.basket.total;
+      })
+      
+      .catch(error => {
+        console.error('Error fetching user data:', error);
+      });
+  }, [userID]);
+  
   const [cardNumber, setCardNumber] = useState('');
   const [cardHolder, setCardHolder] = useState('');
   const [expiryDate, setExpiryDate] = useState('');
@@ -11,38 +43,74 @@ function PaymentPage() {
     e.preventDefault();
     // Ödeme işleme ve validasyon kodları buraya eklenebilir
     console.log({ cardNumber, cardHolder, expiryDate, cvv });
-    if (paymentSuccess) {
-      deleteProductById(productId); // productId should be retrieved from context or props
-    }
   };
 
-  
-
   return (
-    <div className="payment-page">
-      <h1>Payment Information</h1>
-      <form onSubmit={handleConfirmPayment}>
-        <div className="form-group">
+    <div className="payment-container-user">
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Navbar.Brand style={{padding:'20px'}}><Link to="/shoppingPage">Shop</Link></Navbar.Brand>
+            <Nav.Link style={{ textDecoration: 'none', color: 'inherit' }}><Link to="/orderPage"  style={{ textDecoration: 'none', color: 'inherit' }}>My Orders</Link></Nav.Link>
+            <Nav.Link style={{ textDecoration: 'none', color: 'inherit' }}><Link to="/profilePageUser"  style={{ textDecoration: 'none', color: 'inherit' }}>Profile</Link></Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      <h2 className="my-4">Payment Details</h2>
+
+      <form onSubmit={handlePayment}>
+        <div className="form-group-user">
           <label>Card Number</label>
-          <input type="text" value={cardNumber} onChange={(e) => setCardNumber(e.target.value)} />
+          <input
+            type="text"
+            className="form-control-user"
+            value={cardNumber}
+            onChange={(e) => setCardNumber(e.target.value)}
+            placeholder="Enter card number"
+          />
         </div>
-        <div className="form-group">
-          <label>Card Name</label>
-          <input type="text" value={cardName} onChange={(e) => setCardName(e.target.value)} />
+
+        <div className="form-group-user">
+          <label>Card Holder</label>
+          <input
+            type="text"
+            className="form-control-user"
+            value={cardHolder}
+            onChange={(e) => setCardHolder(e.target.value)}
+            placeholder="Enter card holder's name"
+          />
         </div>
-        <div className="form-group">
-          <label>Expiry Date</label>
-          <input type="text" value={expiryDate} onChange={(e) => setExpiryDate(e.target.value)} />
+
+        <div className="form-row-user">
+          <div className="form-group-user col-md-6">
+            <label>Expiry Date</label>
+            <input
+              type="text"
+              className="form-control-user"
+              value={expiryDate}
+              onChange={(e) => setExpiryDate(e.target.value)}
+              placeholder="MM/YY"
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label>CVV</label>
+            <input
+              type="password"
+              className="form-control-user"
+              value={cvv}
+              onChange={(e) => setCvv(e.target.value)}
+              placeholder="CVV"
+            />
+          </div>
         </div>
-        <div className="form-group">
-          <label>CVV</label>
-          <input type="text" value={cvv} onChange={(e) => setCvv(e.target.value)} />
-        </div>
-        <button type="submit">Confirm Payment</button>
+        <p id ='username'></p>
+
+        <button type="submit" className="btn btn-primary-user" onClick={removeFromBasket}>Make Payment</button>
       </form>
     </div>
   );
-
 }
 
 export default PaymentPage;
